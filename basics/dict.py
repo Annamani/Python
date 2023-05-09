ebe7fa13b5be21c1c590e78a5e23235fc49b8b67
annamani <annamanik.vit@gmail.com>
2023-05-09 22:09:34 +0200

    dict file

---
+++ basics/dict.py
@@ -0,0 +1,11 @@
+dict={
+    "first_name":"Annamani",
+    "last_name":"Kamma",
+    "Age": 26,
+    "DOB": "23/01/1997",
+    "Place":"Denmark"
+}
+print(dict)
+print(dict["first_name"])
+print(len(dict))
+print(type(dict))
