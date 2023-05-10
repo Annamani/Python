6256e0e0548663a01d524d41bffd425a9b2edbc8
annamani <annamanik.vit@gmail.com>
2023-05-10 21:30:15 +0200

    new file

--- basics/lists.py
+++ basics/lists.py
@@ -5,3 +5,10 @@ print(fruits[2])
 print(fruits[1:])
 print(fruits[:2])
 print(fruits[:-3])
+numbers=["1","2","3","4","5"]
+print(numbers)
+new_list=[fruits,numbers]
+print(new_list)
+numbers.append(9)
+print(numbers)
+
--- basics/variables.py
+++ basics/variables.py
@@ -5,5 +5,8 @@ a=2
 b=3.4
 print(a+b)
 s1="hello"
-s2="Annamani"
+s2="honey"
 print(s1+" "+s2)
+first_name="Annamani"
+last_name=" Kamma"
+print( first_name+" "+last_name)
