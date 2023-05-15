0ea9c4f124dc2aeceb9b74bd563a4d082b624382
annamani <annamanik.vit@gmail.com>
2023-05-15 22:22:06 +0200

    new file

---
+++ Variables/datatypes.py
@@ -0,0 +1,45 @@
+# #None
+# a
+# print(type(a)) # when we dont assign a value to a variable
+#Numeric
+# We have diff types Int Float Complex bool
+b=2
+print(type(b))
+c=9.76
+print(type(c))
+d=3+4j
+print(type(d))
+e=False
+print(type(e))
+f=complex(2,3)
+print(f)
+print(type(f))
+#List
+g=[1,2,3,4,5,6,7,8,9,10]
+print(g)
+print(type(g))
+#Tuple
+h=(1,2,3,4)
+print(h)
+print(type(h))
+#Set
+i={1,2,3,4,5}
+print(i)
+print(type(i))
+#String
+j="Annamani"
+print(j)
+print(type(j))
+#Range
+k=range(7)
+print(k)
+print(type(k))
+#Dictionary
+l = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(l)
+print(type(l))
+
