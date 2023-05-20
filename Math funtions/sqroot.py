a014f4232647cdc8238caae25e1803b0f040f7bc
annamani <annamanik.vit@gmail.com>
2023-05-20 20:30:00 +0200

    math functions

--- Math funtions/sqroot.py
+++ Math funtions/sqroot.py
@@ -1,11 +1,13 @@
-import math m
-print(math.sqrt(78))
+import math as m
+print(m.sqrt(78))
 # Floor (round off to below number) and Ceil (goes to upper value) values
 x=2.1
-print(math.floor(x))
-print(math.ceil(x))
+print(m.floor(x))
+print(m.ceil(x))
 y = pow(4, 3)
 print(y)
 z = m.pi
 print(z)
+# Trignometry functions
+print(m.cos(45))
 
