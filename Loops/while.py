b822a287908117d1eacabac95ffe35fe9d2f2745
annamani <annamanik.vit@gmail.com>
2023-05-23 21:16:51 +0200

    Loops

--- Loops/while.py
+++ Loops/while.py
@@ -1,6 +1,22 @@
 i = 1
 while i < 6:
   print(i)
-  if i == 3:
-    break
   i += 1
+
+# BReak
+j = 1
+print("Break")
+while j < 6:
+  print(j)
+  if j == 3:
+    break
+  j += 1
+
+#   Continue
+k = 1
+print("Continue")
+while k < 10:
+  k += 1
+  if k == 3:
+    continue
+  print(k)
